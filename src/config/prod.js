78d7968c2b82bf9162653fa4f4cd41b2a20d585a
@BCarrie <bcarrie@premierfarnell.com>
2023-05-11 10:15:51 +0100

    added credentials to prod and railway

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,4 +2,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   jwtSinganiture: process.env.JWT_SINGANITURE,
+  nodemailerSecret: process.env.NODE_MAILER_SECRET,
+  nodemailerUser: process.env.NODE_MAILER_USER,
+  nodemailerPass: process.env.NODE_MAILER_PASS,
 };
